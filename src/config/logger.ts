8fb7e22e0084c98e3c69791d59b918b87d3653eb
Your Full Name <harshal@mamotechnolabs.com>
2026-07-02 23:36:15 +0530

    add winston logger

---
+++ src/config/logger.ts
@@ -0,0 +1,41 @@
+import winston from 'winston'
+import { config } from './index.ts'
+const logger = winston.createLogger({
+    level: 'info',
+    defaultMeta: { service: 'auth-service', env: config.env as string },
+    transports: [
+        new winston.transports.File({
+            level: 'error',
+            dirname: 'logs',
+            filename: 'error.log',
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple(),
+                winston.format.timestamp(),
+            ),
+            silent: config.env === 'development' ? false : true,
+        }),
+        new winston.transports.File({
+            level: 'info',
+            dirname: 'logs',
+            filename: 'combined.log',
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple(),
+                winston.format.timestamp(),
+            ),
+            silent: config.env === 'development' ? false : true,
+        }),
+        new winston.transports.Console({
+            level: 'info',
+            format: winston.format.combine(
+                winston.format.colorize(),
+                winston.format.simple(),
+                winston.format.timestamp(),
+            ),
+            silent: config.env === 'development' ? false : true,
+        }),
+    ],
+})
+
+export default logger
--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
 import app from './app.ts'
 import { config } from './config/index.ts'
+import logger from './config/logger.ts'
 
 function startServer() {
     app.listen(config.port, () => {
-        console.log(
+        logger.info(
             `Server is running on port ${config.port} in ${config.env} mode`,
         )
     })
